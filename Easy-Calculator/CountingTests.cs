cb1e4ddfd5097950c52fbd5560e5fb3823f9fab8
wojpaw <152697729+THEwojpaw@users.noreply.github.com>
2024-05-20 09:26:28 +0200

    Add files via upload

---
+++ Easy-Calculator/CountingTests.cs
@@ -0,0 +1,91 @@
+﻿using Easy_calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCalculator_Tests
+{
+    [TestClass]
+    public class CountingTests
+    {
+        [TestMethod]
+        public void ValidSum()
+        {
+            int a = 8;
+            int b = 3;
+            char operation = '+';
+
+            int sum = Counting.Count(a, b, operation);
+            Assert.AreEqual(11, sum);
+        }
+
+        [TestMethod]
+        public void ValidDifference()
+        {
+            int a = 90;
+            int b = 15;
+            char operation = '-';
+
+            int difference = Counting.Count(a, b, operation);
+            Assert.AreEqual(75, difference);
+        }
+
+        [TestMethod]
+        public void ValidProduct()
+        {
+            int a = 7;
+            int b = 7;
+            char operation = '*';
+
+            int product = Counting.Count(a, b, operation);
+            Assert.AreEqual(49, product);
+        }
+
+        [TestMethod]
+        public void ValidQuotient()
+        {
+            int a = 32;
+            int b = 4;
+            char operation = '/';
+
+            int quotient = Counting.Count(a, b, operation);
+            Assert.AreEqual(8, quotient);
+        }
+        [TestMethod]
+        public void ValidQuotient2()
+        {
+            int a = 32;
+            int b = 4;
+            char operation = ':';
+
+            int quotient2 = Counting.Count(a, b, operation);
+            Assert.AreEqual(8, quotient2);
+        }
+        [TestMethod]
+        public void DivideByZero()
+        {
+            int a = 32;
+            int b = 0;
+            char operation = '/';
+
+            int quotient = Counting.Count(a, b, operation);
+            Assert.AreEqual(0, quotient);
+        }
+
+        [TestMethod]
+        public void NotSign()
+        {
+            int a = 76;
+            int b = 12;
+            var operation = 'a';
+
+            int NotOperation = Counting.Count(a, b, operation);
+            Assert.AreEqual(0, NotOperation);
+        }
+
+
+    }
+}
+
---
+++ Easy-Calculator/ValidateTest.cs
@@ -0,0 +1,30 @@
+﻿using Easy_calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCalculator_Tests
+{
+    [TestClass]
+    public class ValidateTest
+    {
+        [TestMethod]
+        public void String()
+        {
+            string text = "hocus pocus";
+
+            var txt = Validate.ValidateInput(text, out int a);
+            Assert.AreEqual(false, txt);
+        }
+        [TestMethod]
+        public void Int()
+        {
+            string number = "2137";
+
+            var nmbr = Validate.ValidateInput(number, out int b);
+            Assert.AreEqual(true, nmbr);
+        }
+    }
+}
