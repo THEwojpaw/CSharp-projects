19134e8bafd68658c78a3561ea8630f0248b3789
wojpaw <152697729+THEwojpaw@users.noreply.github.com>
2024-05-20 09:24:45 +0200

    Add files via upload

---
+++ Easy-Calculator/Validate.cs
@@ -0,0 +1,16 @@
+﻿namespace Easy_calculator
+{
+    public class Validate
+    {
+        public static bool ValidateInput(string input, out int result)
+        {
+            if (!int.TryParse(input, out result))
+            {
+                Console.WriteLine("Podałeś nieprawidłowy składnik!");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Easy-Calculator/ValidateRG.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Easy_calculator
+{
+    public class ValidateRG
+    {
+        public static bool SignRG(string sign)
+        {
+            Regex rg = new Regex("^[+\\-*\\/:]$");
+            if(!rg.IsMatch(sign))
+            {
+                Console.WriteLine("Wprowadziłeś nieprawidłowy znak (operację)!");
+                return false;
+            }
+       
+            return true;
+        }
+    }
+}
+
