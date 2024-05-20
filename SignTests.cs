c852292597675aabc8f8d820a99df2f23884f5df
wojpaw <152697729+THEwojpaw@users.noreply.github.com>
2024-05-20 09:22:04 +0200

    Add files via upload

---
+++ SignTests.cs
@@ -0,0 +1,34 @@
+﻿using Easy_calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCalculator_Tests
+{
+    [TestClass]
+    public class SignTests
+    {
+        [TestMethod]
+        public void NotSign()
+        {
+            double a = 10;
+            double b = 5;
+            string operation = "a";
+
+            var NotOperation = Counting.Count(a, b, operation);
+            Assert.AreEqual(0, NotOperation);
+        }
+        [TestMethod]
+        public void DoubleSign()
+        {
+            double a = 10;
+            double b = 5;
+            string operation = "**";
+
+            var DoubleOperation = Counting.Count(a, b, operation);
+            Assert.AreEqual(0, DoubleOperation);
+        }
+    }
+}
---
+++ ValidateSign.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Easy_calculator
+{
+    public class ValidateSign
+    {
+        public static bool Sign(string sign)
+        {
+            return sign.Length == 1;
+        }
+    }
+}
