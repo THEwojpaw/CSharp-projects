6c678af653057aff31f65202fef9514b6f27d9a9
wojpaw <152697729+THEwojpaw@users.noreply.github.com>
2024-05-22 08:18:24 +0200

    Add files via upload

---
+++ Counting.cs
@@ -0,0 +1,39 @@
+﻿namespace Kalkulator_txt.BaseOperations
+{
+    public static class Counting
+    {
+        public static double Count(double a, double b, string operation)
+        {
+            try
+            {
+                if (b == 0 & operation == "/")
+                {
+                    Console.WriteLine("Wykryto dzielenie przez zero.");
+                    throw new ArgumentException("Wykryto dzielenie przez zero.");
+                }
+
+                switch (operation)
+                {
+                    case "+":
+                        return a + b;
+                    case "-":
+                        return a - b;
+                    case "*":
+                        return a * b;
+                    case "/":
+                    case ":":
+                        return a / b;
+                    default:
+                        Console.WriteLine("Wykryto nieprawidłowy znak (operację)!");
+                        return 0;
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Wykryto niewłaściwe dane, spróbuj jeszcze raz.");
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ ListCreation.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kalkulator_txt.Extensives
+{
+    public class ListCreation
+    {
+        public static List<string> MatchPatternRangeAndInsertToList(string arguments, string pattern)
+        {
+            var rg = new Regex(pattern);
+            if (rg.IsMatch(arguments))
+            {
+                return rg.Matches(arguments)
+                    .Select(e => e.Value)
+                    .ToList();
+            }
+
+            return new List<string>();
+        }
+
+    }
+}
+
