40492df7d46cec3dae8f508ffa63c028f58660bb
wojpaw <152697729+THEwojpaw@users.noreply.github.com>
2024-05-22 08:25:26 +0200

    Add files via upload

---
+++ Calculator-txt/program/Calculations.cs
@@ -0,0 +1,16 @@
+﻿namespace Kalkulator_txt.BaseOperations
+{
+    public static class Calculations
+    {
+        public static double Calculate(string operation, List<int> digits)
+        {
+            double result = digits[0];
+
+            foreach (var digit in digits.Skip(1))
+            {
+                result = Counting.Count(result, digit, operation);
+            }
+            return result;
+        }
+    }
+}
---
+++ Calculator-txt/program/ValidateInput.cs
@@ -0,0 +1,15 @@
+﻿namespace Easy_calculator.Validations
+{
+    public class ValidateInput
+    {
+        public static bool ValidateInputType(string input, out int result)
+        {
+            if (!int.TryParse(input, out result))
+            {
+                Console.WriteLine("Podałeś nieprawidłowy składnik!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
